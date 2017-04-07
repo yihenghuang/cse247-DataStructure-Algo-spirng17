53ffff3bf4d442534037503f18af94e828658731
Administrator <Administrator@windows10.microdone.cn>
2017-04-06 23:09:26 -0500

    late coupon used

--- labs/kwaymergesort/KWayMergeSort.java
+++ labs/kwaymergesort/KWayMergeSort.java
@@ -3,36 +3,81 @@ package kwaymergesort;
 import timing.Ticker;
 
 public class KWayMergeSort {
-	
-	/**
-	 * 
-	 * @param K some positive power of 2.
-	 * @param input an array of unsorted integers.  Its size is either 1, or some other power of 2 that is at least K
-	 * @param ticker call .tick() on this to account for the work you do
-	 * @return
-	 */
+
 	public static Integer[] kwaymergesort(int K, Integer[] input, Ticker ticker) {
 		int n = input.length;
-		
-		//
-		// FIXME
-		// Following just copies the input as the answer
-		//
-		// You must replace the loop below with code that performs
-		// a K-way merge sort, placing the result in ans
-		//
-		// The web page for this assignment provides more detail.
-		//
-		// Use the ticker as you normally would, to account for
-		// the operations taken to perform the K-way merge sort.
-		//
-		Integer[] ans = new Integer[n];
-		for (int i=0; i < n; ++i) {
-			ans[i] = input[i];
-			ticker.tick();
+
+	if (n<=1){
+		return input;
+	}
+	
+
+		Integer[][] split = new Integer[K][n/K];
+
+		int i=0;
+		int j=0;
+		int k=0;
+		for (i=0; i<K; i++){
+			for (j=0; j<n/K; j++){
+				split[i][j]=input[k];
+				k++;
+			}
 		}
-		
+		n=n/K;
+		for (i=0; i<K; i++){
+			split[i]=kwaymergesort(K, split[i], ticker);
+		}
+
+		Integer[] ans = mergerecursive(split, ticker)[0];
 		return ans;
+	}
+
+
+	public static Integer[][] mergerecursive(Integer[][] splits, Ticker ticker) {
+		
+		for (int j=splits.length; j>1; j=j/2){
+			for (int i=0; i<j;i+=2){
+				splits[i/2]=mergeTwo(splits[i],splits[i+1],ticker);
+			}
+		
+		}
+		return splits;
+	}
+	
+	public static Integer[] mergeTwo(Integer[] input1, Integer[] input2, Ticker tick) {
+		Integer[] ans = new Integer[input1.length+input2.length];
+		int a=0;
+		int b=0; 
+		int c=0; 
+		while((a<input1.length)&&(b<input2.length)){
+			if(input1[a]<input2[b]){
+				ans[c]= input1[a]; 
+				a++; 
+				c++; 
+			}
+			else {
+				ans[c]= input2[b]; 
+				b++; 
+				c++; 
+			}
+
+		}
+		while(a<input1.length){
+			ans[c]=input1[a]; 
+			c++;
+			a++; 
+			
+		}
+		while(b<input2.length){
+			ans[c]=input2[b]; 
+			c++;
+			b++; 
+			
+		}
+		return ans; 
+
+
+
 	}
 
 }
